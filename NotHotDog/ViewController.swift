8c430fda1af4ba63ad6e6eaf92eba94ebe117c91
Nikita Pokidyshev <nik96lip@gmail.com>
2017-11-09 20:36:18 +0300

    Initial Commit

---
+++ NotHotDog/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  NotHotDog
+//
+//  Created by Nikita Pokidyshev on 09/11/2017.
+//  Copyright © 2017 Nikita Pokidyshev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
